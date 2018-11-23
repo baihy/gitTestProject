77df4b790852387dbe792a1bd27c0f902268da79
baihuayang <1053091115@qq.com>
2018-11-23 14:21:04 +0800

    idea代码提交测试

--- src/main/java/com/zskj/httpclient/App.java
+++ src/main/java/com/zskj/httpclient/App.java
@@ -1,21 +1,20 @@
 package com.zskj.httpclient;
 
+import com.zskj.utils.HttpClientUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import com.zskj.utils.HttpClientUtils;
-
 /**
  * Hello world!
- *
  */
 public class App {
-	public static void main(String[] args) {
-		String url = "http://127.0.0.1:8080/v1/hello/put";
-		Map<String, String> paramMap = new HashMap<>();
-		// String result = HttpClientUtils.post(url, paramMap);
-		paramMap.put("id", "abc");
-		String result = HttpClientUtils.put(url, paramMap);
-		System.out.println(result);
-	}
+    public static void main(String[] args) {
+        String url = "http://127.0.0.1:8080/v1/hello/put";
+        Map<String, String> paramMap = new HashMap<>();
+        // String result = HttpClientUtils.post(url, paramMap);
+        paramMap.put("id", "abc");
+        String result = HttpClientUtils.put(url, paramMap);
+        System.out.println("返回结果：" + result);
+    }
 }
