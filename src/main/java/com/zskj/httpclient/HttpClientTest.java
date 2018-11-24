b480293acbd4e241d0bc8ed05a65b842e43bf603
baihuayang <1053091115@qq.com>
2018-11-24 14:03:42 +0800

    大

--- src/main/java/com/zskj/httpclient/HttpClientTest.java
+++ src/main/java/com/zskj/httpclient/HttpClientTest.java
@@ -4,17 +4,18 @@ import com.zskj.utils.HttpClientUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Hello world!
  */
 public class HttpClientTest {
-    public static void main(String[] args) {
-        String url = "http://127.0.0.1:8080/v1/hello/put";
-        Map<String, String> paramMap = new HashMap<>();
-        // String result = HttpClientUtils.post(url, paramMap);
-        paramMap.put("id", "abc");
-        String result = HttpClientUtils.put(url, paramMap);
-        System.out.println("返回结果：" + result);
-    }
+	public static void main(String[] args) {
+		String url = "http://127.0.0.1:8080/v1/hello/123";
+		Map<String, String> paramMap = new HashMap<>();
+		for (int i = 0; i < 10000; i++) {
+			String result = HttpClientUtils.post(url, paramMap);
+			System.out.println(Thread.currentThread().getName() + "：" + result);
+		}
+	}
 }
