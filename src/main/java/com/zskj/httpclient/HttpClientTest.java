629b882e1888a3ab6bb9e41f2be22e87364d9149
baihuayang <1053091115@qq.com>
2018-11-24 21:11:53 +0800

    添加了多线程

--- src/main/java/com/zskj/httpclient/HttpClientTest.java
+++ src/main/java/com/zskj/httpclient/HttpClientTest.java
@@ -1,10 +1,9 @@
 package com.zskj.httpclient;
 
-import com.zskj.utils.HttpClientUtils;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
+
+import com.zskj.utils.HttpClientUtils;
 
 /**
  * Hello world!
@@ -13,7 +12,7 @@ public class HttpClientTest {
 	public static void main(String[] args) {
 		String url = "http://127.0.0.1:8080/v1/hello/123";
 		Map<String, String> paramMap = new HashMap<>();
-		for (int i = 0; i < 10000; i++) {
+		for (int i = 0; i < 2; i++) {
 			String result = HttpClientUtils.post(url, paramMap);
 			System.out.println(Thread.currentThread().getName() + "：" + result);
 		}
